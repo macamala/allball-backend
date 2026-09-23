274abd528e2ae083aa733fb9ec2b9a369d6d8f39
macamala <79318740+macamala@users.noreply.github.com>
2026-09-23 21:15:24 +1000

    Cover expanded team-sport fixture targets

--- tests/test_thesportsdb_schedule.py
+++ tests/test_thesportsdb_schedule.py
@@ -2,5 +2,17 @@ from collector.thesportsdb_schedule import JOB_KEY, TARGET_SPORTS
 
 
 def test_known_league_schedule_targets_main_team_sports():
-    assert JOB_KEY == "thesportsdb-known-league-fixtures-v1"
-    assert set(TARGET_SPORTS) == {"handball", "volleyball", "ice-hockey", "baseball"}
+    assert JOB_KEY == "thesportsdb-known-league-fixtures-v2"
+    assert set(TARGET_SPORTS) == {
+        "handball",
+        "volleyball",
+        "ice-hockey",
+        "baseball",
+        "basketball",
+        "american-football",
+        "rugby",
+        "rugby-league",
+        "netball",
+        "australian-rules",
+        "cricket",
+    }
