421a793b53ac780fb1391eafed14275671b7a8af
macamala <79318740+macamala@users.noreply.github.com>
2026-09-24 22:11:36 +1000

    Run WTA country repair on upgraded breadth job

--- collector/wta_breadth.py
+++ collector/wta_breadth.py
@@ -31,7 +31,7 @@ from collector.util import dump_json, load_json, parse_datetime, slugify
 
 logger = logging.getLogger(__name__)
 
-JOB_KEY = "wta-global-breadth-v6"
+JOB_KEY = "wta-global-breadth-v7"
 SOURCE_ID = "wta-global"
 PUBLIC_BREADTH_STATUS = "single-source-breadth"
 
